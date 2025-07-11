ba6f0da6a7e369c968650475bbc3cc5e0cb20bd8
Vin <chokun5588@gmail.com>
2025-07-11 15:02:45 +0700

    add file

--- cmd/cls01/main.go
+++ cmd/cls01/main.go
@@ -2,8 +2,34 @@ package main
 
 import "fmt"
 
+// For Loop and While Loop in Go lang
 func main() {
-	fmt.Println("This is Calculator")
-	
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("For Loop:", i)
+	}
+
+	// While Loop in Go is implemented using a for loop without initialization and post statement
+	N := 0
+	for N != 0 {
+		fmt.Println("While Loop:", N)
+		N++
+		if N == 5 {
+			break
+		}
+	}
+
+	list := []int{1, 2, 3, 4, 5}
+	for i := 0; i < len(list); i++ {
+		fmt.Println("List item:", list[i])
+	}
+
+	for _, item := range list {
+		fmt.Println("List item using range:", item)
+	}
+
+	for i, item := range list {
+		fmt.Printf("List item with index %d: %d\n", i, item)
+	}
+
 }
- 
